0998e40b2d13dc83a5aed40570691405162fd8b2
Prasath Thamboo <prasath1@hotmail.fr>
2024-02-12 22:49:03 +0100

    third

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const salut = "salut"
+const a = 3; 
+const note = [
+    13, 12, 10, 17
+]
+const person = {
+    firstname: "john",
+    lastname : "pras", 
+    age : 24, 
+    mail: "bingo@hotmail.fr" 
+}
+
+
+console.log(a)
