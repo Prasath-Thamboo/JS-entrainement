13f9d9520b826a9039312ca984730e48fae1cda3
Navilone <prasath.thamboo@navilone.com>
2024-10-25 09:44:23 +0200

    First

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function canDrive(age, pays){
-if (age < 18 && pays === "FR" || age < 16 && pays === "US"){
-    return false
-}{
-    return true
-}    
-}
-console.log(canDrive(18, "FR"))
+const salut = "c/'est moi"
+
+
+console.log(salut)
