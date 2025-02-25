f121f4cca4db901eeda5964452f3723641ac2b86
Navilone <70574108+Prasath-Thamboo@users.noreply.github.com>
2025-02-25 16:10:23 +0100

    fonctions

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-const films = ["Lilo", "Matrix", "Evil Dead"]
-const age = prompt ("Quel est votre age ")
-
-if (!isNaN(age)) {
-    age = Number(age); 
-    if (age >= 18) {
-        console.log("Regarde Evil et tout ce que tu veux")
-    } 
-    
-    else if (age < 18 && age >= 12){
-        console.log("Regarde Matrix ou lilo")
-    } 
-    
-    else {
-        console.log("Regarde QUE Lilo")
+const a = {
+    firstname: "John", 
+    lastname: "Doe", 
+    fullname: function (){
+        console.log(this)
     }
-} else {
-    console.log("Zingo")
 }
+
+a.fullname()
